47d340bdba8d5130f9f3c74b667542a4dff42d9e
Jack Monas <jack.monas@1x.tech>
2024-08-27 14:30:16 -0700

    debug

--- calc_fvd.py
+++ calc_fvd.py
@@ -1,5 +1,5 @@
 from cdfvd import fvd
-evaluator = fvd.cdfvd('i3d', ckpt_path=None)
+evaluator = fvd.cdfvd('i3d', ckpt_path=None, device="cpu")
 gen_path = "../Vista/logs_256/2024-08-15T13-09-45_training-vista_phase1_fps5_frames25_dataset1xgpt_competition/images/train/samples_mp4"
 gt_path = "../Vista/logs_256/2024-08-15T13-09-45_training-vista_phase1_fps5_frames25_dataset1xgpt_competition/images/train/inputs_mp4"
 evaluator.compute_real_stats(evaluator.load_videos(gt_path, resolution=256, sequence_length=25, data_type='video_folder', conditioning_frames=6, subset_num = 50))
